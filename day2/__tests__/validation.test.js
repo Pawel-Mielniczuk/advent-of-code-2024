a1f82ba05b9865a5d463057fb654517b93a2724c
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-17 20:45:11 +0100

    add vites into project

---
+++ day2/__tests__/validation.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { isDecreasingSequence, isIncreasingSequence, isStrictlyMonotonicSequence } from "./validation.js";
+
+describe("sum two numbers", () => {
+  it("should correctly add two numbers", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
