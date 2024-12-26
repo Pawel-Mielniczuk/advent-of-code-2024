dd8371f8e9c522712fe6bb2fdfb3617516dd5714
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-26 18:37:07 +0100

    add solution for part two

--- day3/index.ts
+++ day3/index.ts
@@ -7,9 +7,10 @@ async function createInput(): Promise<string> {
 async function scanUncorruptedMemory(): Promise<void> {
   const data = await createInput();
 
+  // --Part One --
   const multiplicationExpressions: string[] = data.match(/mul\(\d+,\d+\)/g) || [];
 
-  let result = 0;
+  let partOneResult = 0;
 
   const parsedNumbers: number[][] = multiplicationExpressions.map((elem) => {
     const item = elem.match(/\d+/g)?.map((num) => Number(num)) ?? [];
@@ -18,11 +19,38 @@ async function scanUncorruptedMemory(): Promise<void> {
 
   const products = parsedNumbers.map((operandPair: number[]) => operandPair[0] * operandPair[1]);
 
-  result = products.reduce((acc: number, curr: number) => {
+  partOneResult = products.reduce((acc: number, curr: number) => {
     return acc + curr;
   });
 
-  console.log("result", result);
+  console.log("partOneResult", partOneResult);
+
+  //--Part two--
+
+  function calculateActiveMultiplications(): number {
+    let isActive = true;
+    let result = 0;
+
+    const regex = /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g;
+    let match: RegExpExecArray | null;
+
+    while ((match = regex.exec(data)) !== null) {
+      if (match[0] === "do()") {
+        isActive = true;
+      } else if (match[0] === "don't()") {
+        isActive = false;
+      } else if (isActive) {
+        const num1 = Number(match[1]);
+        const num2 = Number(match[2]);
+        result += num1 * num2;
+      }
+    }
+    return result;
+  }
+
+  const partTwoResult = calculateActiveMultiplications();
+
+  console.log("partTwoResult:", partTwoResult);
 }
 
 scanUncorruptedMemory();
