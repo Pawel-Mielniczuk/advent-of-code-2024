4571e2dc74a265e10b41e27cc5b1ae5d8f99752b
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-27 11:06:39 +0100

    add findForwardAndBackward function

--- day4/index.ts
+++ day4/index.ts
@@ -1,9 +1,42 @@
+import { count } from "console";
 import fs from "fs/promises";
 
 async function createInput(): Promise<string> {
-  return await fs.readFile("./day3/input.txt", "utf8");
+  return await fs.readFile("./input.txt", "utf8");
 }
 
-async function findSentenceInGrid() {}
+const mockedGrid = [
+  ["MMMSXXMASM"],
+  ["MSAMXMSMSA"],
+  ["AMXSXMAAMM"],
+  ["MSAMASMSMX"],
+  ["XMASAMXAMM"],
+  ["XXAMMXXAMA"],
+  ["SMSMSASXSS"],
+  ["SAXAMASAAA"],
+  ["MAMMMXMMMM"],
+  ["MXMXAXMASX"],
+];
+
+async function findSentenceInGrid() {
+  const data = await createInput();
+  const characterGrid = data.split("\n").map((row) => [row]);
+  const SEARCH_WORD = /XMAS/;
+  const BACKWARD_SEARCH_WORD = /SAMX/;
+  let count = 0;
+
+  function findForwardAndBackward() {
+    for (let i = 0; i < mockedGrid.length; i++) {
+      const row = mockedGrid[i][0];
+      if (row.match(SEARCH_WORD)) {
+        count += 1;
+      }
+      if (row.match(BACKWARD_SEARCH_WORD)) {
+        count += 1;
+      }
+    }
+  }
+  findForwardAndBackward();
+}
 
 findSentenceInGrid();
