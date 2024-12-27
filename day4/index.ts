d0c7fc8e270d899a9a29bd48db9e6fba0f92e2a3
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-27 12:14:40 +0100

    implement grid transposition for multi-directional search

--- day4/index.ts
+++ day4/index.ts
@@ -1,4 +1,3 @@
-import { count } from "console";
 import fs from "fs/promises";
 
 async function createInput(): Promise<string> {
@@ -18,6 +17,43 @@ const mockedGrid = [
   ["MXMXAXMASX"],
 ];
 
+function transformArrayOfStringIntoArrayOfChar(array) {
+  const transformArray = array.map((row) => {
+    const item = row[0].split("");
+    return item;
+  });
+  return transformArray;
+}
+
+function mergeCharactersIntoArray(array: string[][]): string[][] {
+  return array.map((innerArr) => [innerArr.join("")]);
+}
+
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+//
+// 0 1 2 => index 0
+// 0 1 2 => index 1
+// 0 1 2 => index 2
+
+function transposeGrid(grid: string[][]): string[][] {
+  const transposedGrid: string[][] = grid.map(() => {
+    return [];
+  });
+
+  const gridLength = grid.length;
+  for (let i = 0; i < gridLength; i++) {
+    const row = grid[i];
+    for (let j = 0; j < row.length; j++) {
+      transposedGrid[j].push(row[j]);
+    }
+  }
+  return transposedGrid;
+}
+
 async function findSentenceInGrid() {
   const data = await createInput();
   const characterGrid = data.split("\n").map((row) => [row]);
@@ -25,9 +61,9 @@ async function findSentenceInGrid() {
   const BACKWARD_SEARCH_WORD = /SAMX/;
   let count = 0;
 
-  function findForwardAndBackward() {
-    for (let i = 0; i < mockedGrid.length; i++) {
-      const row = mockedGrid[i][0];
+  function findForwardAndBackward(arr: string[][]): void {
+    for (let i = 0; i < arr.length; i++) {
+      const row = arr[i][0];
       if (row.match(SEARCH_WORD)) {
         count += 1;
       }
@@ -36,7 +72,13 @@ async function findSentenceInGrid() {
       }
     }
   }
-  findForwardAndBackward();
+  findForwardAndBackward(mockedGrid);
+  const transformedArray = transformArrayOfStringIntoArrayOfChar(mockedGrid);
+  const transposedGrid = transposeGrid(transformedArray);
+  const mergedCharInArr = mergeCharactersIntoArray(transposedGrid);
+  findForwardAndBackward(mergedCharInArr);
+
+  console.log("count", count);
 }
 
 findSentenceInGrid();
